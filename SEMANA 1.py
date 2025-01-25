e2e0682ee968f3ac303bd4b59c29953ff602cd1d
PC Yair <yairveratec@gmail.com>
2025-01-24 19:43:24 -0600

    SEMANA 1

---
+++ SEMANA 1.py
@@ -0,0 +1,45 @@
+#SEMANA 1
+
+#Ejercicio 1
+# ¿Cuál es la función de la memoria secundaria en una computadora?
+#Respuesta: C
+
+#Ejercicio 2
+# ¿Qué es un programa?
+# Respuesta: Es un conjunto de indicaciones en un lenguaje de programación para realizar una tarea especifica
+
+#Ejercicio 3
+# ¿Cuál es la diferencia entre un compilador y un interprete?
+# Respuesta: el compilador recibe y guarda los datos y el interprete los trasnforma de modo que el lenguaje los entienda
+
+#Ejercicio 4
+# ¿Cuál de los siguientes contiene "codigo maquina"?
+# Respuesta: c
+
+#Ejercicio 5
+# ¿Qué está mal en el siguiente código?
+# Respuesta: "primt" en lugar de "print"
+
+#Ejercicio 6
+# ¿En qué lugar del computador queda almacenada una variable, como en este caso "x", despues de ejecutar la siguiente linea de Python?:
+    # x = 123
+#Respuesta: C
+
+#Ejercicio 7
+#¿Qué mostrará en pantalla el siguiente programa?
+# x = 43
+# x = x + 1
+# print(x)
+#Respuesta: B
+
+#Ejercicio 8
+# Explica en cada uno de los siguientes conceptos usando un ejemplo de una capacidad humana:
+# 1. Unidad central de procesamiento = El cerebro humano
+# 2. Memoria principal = memoria a largo plazo del humano
+# 3. Memoria secundaria = es la memoria que almacena recuerdos por un periodo corto de tiempo
+# 4. Dispositivos de entrada = el proceso de adquisición de recuerdos
+# 5. Dispositivos de salida = cuando contamos nuestros recuerdos
+
+#Ejercicio 9
+# ¿Cómo puedes corregir un "Error de sintaxis"?
+# Respuesta: con la función "corrección rápida"
