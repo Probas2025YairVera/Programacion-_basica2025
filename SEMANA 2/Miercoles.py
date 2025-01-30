5e0717ae28b6b468ce188bc0cd3417f40f6f0106
PC Yair <yairveratec@gmail.com>
2025-01-30 16:17:19 -0600

    miercoles

--- SEMANA 2/Miercoles.py
+++ SEMANA 2/Miercoles.py
@@ -1,2 +1,33 @@
 #Tipos de datos estructurados compuestos
 
+meses = ['enero', 'febrero','marzo', 'abril', 'mayo', 'junio', 'julio', 'agosto', 'septiembre', 'octubre', 'noviembre', 'diciembre']
+
+enumerate(meses)
+
+for mes in enumerate(meses,1):
+    print(mes)
+
+for j in range(1,25,2):
+    print(j)
+
+
+#Operaciones:
+
+#Aritmeticos
+#(x)
+#x^2
+#x * y
+#x / y
+#x + y
+#x - y
+
+#Logicas
+#And  
+#Or
+#Not
+
+#Relacionales
+#<, >
+#>=  <=
+#==
+#!=  
