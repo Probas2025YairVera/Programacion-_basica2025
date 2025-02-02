dcc94e76d219bb1482a3bb83d94e7addf2f61008
PC Yair <yairveratec@gmail.com>
2025-02-01 18:42:34 -0600

    p3

--- Problemas_Python/Problema_3.py
+++ Problemas_Python/Problema_3.py
@@ -2,13 +2,17 @@
 #3 Calcular el factorial de un numero dado
 
 num = int(input('intoduzca un numero: '))
+fact=1
+i = 1
+for i in range(num):
+    fact = fact * i
+    i = i + 1
+print(fact)
 
-def factorial(num):
-    if num == 0 or num == 1:
-        resultado = 1
-    elif num > 1:
-        resultado = num * factorial(num - 1)
-    return resultado
+
+
+
+
 
 
 
