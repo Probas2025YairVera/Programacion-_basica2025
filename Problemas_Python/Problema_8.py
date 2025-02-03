5f6602893592803276b2f27c21ef1699bf1b05c0
Laptop Nueva <yairveratec@gmail.com>
2025-02-02 19:23:58 -0600

    problema8

--- Problemas_Python/Problema_8.py
+++ Problemas_Python/Problema_8.py
@@ -1,3 +1,10 @@
-#Problema 8
+#8 Calcular el area y la circunferencia de un circulo
 
-print("Hola Mundo")
+r = float(input('ingrese el radio del circulo: '))
+
+Area = 3.1416 * (r) ** 2
+
+Circ = 3.1416 * (r * 2)
+
+print('El area es: ', Area)
+print('La circunferencia es: ', Circ)
