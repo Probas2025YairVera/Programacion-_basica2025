1b3760ff9af0263dca09e80dd06b4e9229f18075
Laptop Nueva <yairveratec@gmail.com>
2025-02-02 19:17:19 -0600

    problema 7

--- Problemas_Python/Problema_7.py
+++ Problemas_Python/Problema_7.py
@@ -1,1 +1,8 @@
-#7 Problema 7
+#7 compruebe si un numero es par, impar o multiplo de otro
+
+num = int(input('ingrese un numero: '))
+
+if num%2 == 0:
+    print(num, 'es par')
+else:
+    print(num, 'es impar')
