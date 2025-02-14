c07e721280bc0c1aa9387e4e613f7ab0059b8f61
Laptop Nueva <yairveratec@gmail.com>
2025-02-14 12:17:26 -0600

    problema15

--- Problemas_10_al_20/Problema15.py
+++ Problemas_10_al_20/Problema15.py
@@ -4,6 +4,9 @@ año = int(input('Indique un año: '))
 
 if (año % 4 == 0 and año % 100 != 0) or (año % 400 == 0):
     print(f'{año} es un año bisiesto')
+else:
+    print(f'{año} no es bisiesto')
+    
 
     
        
