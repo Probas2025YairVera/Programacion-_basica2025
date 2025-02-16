e1aa021d9183556a026498a1f7c25ed89dd56ca1
Laptop Nueva <yairveratec@gmail.com>
2025-02-15 23:24:46 -0600

    problema17

---
+++ Problemas_10_al_20/Problema17.py
@@ -0,0 +1,21 @@
+#Problema 17
+
+#PILAS
+
+pila = [1, 2 ,3]
+#Agregar elementos por el final de la pila(lista)
+pila.append(4)
+pila.append(5)
+print(pila)
+#Quitar elemntos por el final de la pila(lista)
+n = pila.pop()
+print(f'Sacando el elemento {n}')
+print(pila)
+
+
+#COLAS
+
+cola = ["Hola", "Profe", "Paseme", "Por favor", "Calculo", "Me", "Mata"]
+#Igual agregamos elementos al final con append
+
+cola.append("Ya no puedo")
