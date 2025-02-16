a9cc9daebd8b4812fe8d0b7870befc57d3b44369
Laptop Nueva <yairveratec@gmail.com>
2025-02-15 23:13:58 -0600

    problema14

--- Problemas_10_al_20/Problema14.py
+++ Problemas_10_al_20/Problema14.py
@@ -1,2 +1,32 @@
 #14 Implementar distintos metodos de ordenamiento
 
+#Algoritmos de ordenamiento
+'''
+lista = [5,7,15,6,84,2,4,1]
+
+longitud = len(lista)
+
+for i in range(longitud - 1):
+    menor = i
+    for j in range (i + 1, longitud):
+        if lista[j] < lista[menor]:
+            menor = j
+    temporal = lista[menor]
+    lista[menor] = lista[i]
+    lista[i] = temporal
+
+print(lista)
+'''
+
+'''
+lista = [5,7,15,6,84,2,4,1]
+
+for i in range(len(lista) - 1):
+    for j in range(len(lista) - 1):
+        if lista[j] > lista[j + 1]:
+            temporal = lista[j]
+            lista[j] = lista[j + 1]
+            lista[j + 1] = temporal
+
+print(lista)
+'''
