8fbe8f013065d219c326180a087e7601f85038d6
Laptop Nueva <yairveratec@gmail.com>
2025-04-13 12:48:11 -0600

    encapsulamientos

---
+++ Notas/Estructuras de datos/Funciones/Encapsulamiento.py
@@ -0,0 +1,6 @@
+#Encapsulamiento
+
+#El encapsulamiento o encapsulación en programación es un concepto relacionado con la programación orientada 
+# a objetos, y hace referencia al ocultamiento de los estado internos de una clase al exterior. Dicho de otra manera,
+#  encapsular consiste en hacer que los atributos o métodos internos a una clase no se puedan acceder ni modificar desde 
+# fuera, sino que tan solo el propio objeto pueda acceder a ellos.
