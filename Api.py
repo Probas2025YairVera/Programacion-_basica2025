79cc07b5b55db42d9056c332430cba719cf3725c
Laptop Nueva <yairveratec@gmail.com>
2025-05-07 22:46:09 -0600

    API

--- Api.py
+++ Api.py
@@ -7,7 +7,36 @@ URL = "https://rickandmortyapi.com/api"              #Link principal
 URLP = "https://rickandmortyapi.com/api/character"   #Link de los personajes de la serie    
 URLE = "https://rickandmortyapi.com/api/episode"     #link de los episodios
 
-respusta = requests.get(URL)
+#PERSONAJES
+respusta = requests.get(URLP)
 datos = respusta.json()
-print(datos)
-    
+print("--------PERSONAJES--------")
+for result in datos["results"]:
+    print(result["name"])
+
+
+
+#EPISODIOS
+
+respuesta2 = requests.get(URLE)
+datos2 = respuesta2.json()
+print("-------EPISODIOS-------")
+for results_ in datos2["results"]:
+    print(results_["name"],"-",results_["air_date"])
+
+
+genero = input("Ingrese el genero de los personajes\nMale = hombre\nFemale = mujer\n")
+
+if genero == "Male":
+    print("-------PERSONAJES MASCULINOS-------")
+    for result in datos["results"]:
+        if result["gender"] == "Male":
+            print(result["name"])
+else:
+    print("-------PERSONAJES FEMENINOS-------")
+    for result in datos["results"]:
+        if result["gender"] == "Female":
+            print(result["name"])
+
+        
+
