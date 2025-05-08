e91b8a4240e4442b4375479cebb769328e9d7c48
Laptop Nueva <yairveratec@gmail.com>
2025-05-08 13:37:51 -0600

    Api

--- Api.py
+++ Api.py
@@ -11,8 +11,11 @@ URLE = "https://rickandmortyapi.com/api/episode"     #link de los episodios
 respusta = requests.get(URLP)
 datos = respusta.json()
 print("--------PERSONAJES--------")
+Per_dict=[]                             #En Per_dict tengo guardados los personajes en forma de lista
 for result in datos["results"]:
-    print(result["name"])
+    Per_dict.append(result["name"])
+
+print(Per_dict)
 
 
 
@@ -22,21 +25,31 @@ respuesta2 = requests.get(URLE)
 datos2 = respuesta2.json()
 print("-------EPISODIOS-------")
 for results_ in datos2["results"]:
-    print(results_["name"],"-",results_["air_date"])
+    print(results_["name"],"-",results_["episode"])
 
-
-genero = input("Ingrese el genero de los personajes\nMale = hombre\nFemale = mujer\n")
-
+genero = input("----Ingrese el genero de los personajes----\nMale = hombre\nFemale = mujer\n")
+Per_Male = []       #lista de personajes masculinos
+Per_Female = []     #lista de personajes femeninos
 if genero == "Male":
     print("-------PERSONAJES MASCULINOS-------")
     for result in datos["results"]:
         if result["gender"] == "Male":
-            print(result["name"])
+            Per_Male.append(result["name"])
+            
 else:
     print("-------PERSONAJES FEMENINOS-------")
     for result in datos["results"]:
         if result["gender"] == "Female":
-            print(result["name"])
+            Per_Female.append(result["name"])
+if genero == "Male":
+    print(Per_Male)
+else:
+    print(Per_Female)
 
-        
 
+Per_Esp = {"Humanos":[], "Aliens":[]}
+for Humano in datos["results"]:
+    if result["species"] == "Human":
+        Per_Esp["Humanos"].append(result["name"])
+
+print(Per_Esp)
