7dea8ed836178ee05a7d15e10226f62d58b17500
Laptop Nueva <yairveratec@gmail.com>
2025-05-08 17:40:14 -0600

    Api

---
+++ Script2.py
@@ -0,0 +1,24 @@
+with open("Personajestotales.txt", "r") as archivo:
+    contenido = archivo.read()
+    Totales=contenido
+
+with open("Masculinos.txt", "r") as archivo:
+    contenido = archivo.read()
+    Masculinos = contenido
+
+with open("Femeninos.txt", "r") as archivo:
+    contenido = archivo.read()
+    Femeninos = contenido
+
+with open("Estatus.txt", "r") as archivo:
+    contenido = archivo.read()
+    Estatus = contenido
+
+with open("Especie.txt", "r") as archivo:
+    contenido = archivo.read()
+    Especie = contenido
+
+
+
+
+
